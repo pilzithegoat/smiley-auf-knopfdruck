e1c4bece24dfb1391be10d1b82ab741bcdc2f497
Julian <81559529+pilzithegoat@users.noreply.github.com>
2025-02-11 09:04:29 +0100

    Knöpfe hinzugefügt

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
 basic.forever(function () {
 	
 })
